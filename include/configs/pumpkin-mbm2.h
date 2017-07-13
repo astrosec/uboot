3962d4dbcdc7c39f9da76401ad880e76f954a5be
Catherine <catherine@kubos.co>
2017-07-13 10:23:10 -0500

    Creating common config file for kubos update

--- include/configs/pumpkin-mbm2.h
+++ include/configs/pumpkin-mbm2.h
@@ -17,15 +17,37 @@
 #pragma once
 
 #include "am335x_evm.h"
+#include "kubos-common.h"
 
 /* Undo things we don't want to include from the base Beaglebone Black configuration */
 #undef CONFIG_SYS_LDSCRIPT /* For NOR flash, which we (and the BBB) don't support */
 #undef CONFIG_BOOTCOMMAND
+#undef CONFIG_EXTRA_ENV_SETTINGS
+/* End of undefs */
+
+/* File updates */
+#ifdef CONFIG_UPDATE_KUBOS
+#define CONFIG_SYS_DFU_DATA_BUF_SIZE 500 * SZ_1K /* File transfer chunk size */
+#define CONFIG_SYS_DFU_MAX_FILE_SIZE 4 * SZ_1M   /* Maximum size for a single file.  Currently zImage (~2.5M) */
+
+/* DFU Configuration */
+#define DFU_ALT_INFO_MMC \
+	"dfu_alt_info_mmc=" 		\
+	"kernel fat 0 5;" 		\
+	"rootfs part 0 6\0"
+
+#define DFU_ALT_INFO_NOR \
+	"dfu_alt_info_nor="		    \
+	"uboot raw 0xA000 0x56000;" \
+	"dtb raw 0x70000 0x10000" \
+	"\0"
+#else
+#define DFU_ALT_INFO_MMC ""
+#define DFU_ALT_INFO_NOR ""
+#endif
 
 #define CONFIG_BOOTCOMMAND \
 	"run distro_bootcmd"
-
-#undef CONFIG_EXTRA_ENV_SETTINGS
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	DEFAULT_LINUX_BOOT_ENV \
@@ -53,57 +75,7 @@
 		"fi;\0" \
 	NETARGS \
 	DFUARGS \
-	BOOTENV
+	BOOTENV \
+	KUBOS_UPDATE_ARGS
 #endif
 
-/* File updates */
-#ifdef CONFIG_UPDATE_KUBOS
-#define CONFIG_SYS_DFU_DATA_BUF_SIZE 500 * SZ_1K /* File transfer chunk size */
-#define CONFIG_SYS_DFU_MAX_FILE_SIZE 4 * SZ_1M   /* Maximum size for a single file.  Currently zImage (~2.5M) */
-#endif
-
-/* Update Definitions */
-#ifdef CONFIG_UPDATE_KUBOS
-
-#define KUBOS_CURR_VERSION "kubos_curr_version"
-#define KUBOS_PREV_VERSION "kubos_prev_version"
-#define KUBOS_CURR_TRIED   "kubos_curr_tried"
-#define KUBOS_BASE         "kpack-base.itb"
-#define KUBOS_UPDATE_FILE  "kubos_updatefile"
-
-#endif
-
-#ifdef TEMP_FAKE_DEFINE
-#ifdef CONFIG_UPDATE_KUBOS
-
-/* DFU Configuration */
-#define DFU_ALT_INFO_MMC \
-	"dfu_alt_info_mmc=" 		\
-	"kernel fat 0 5;" 		\
-	"rootfs part 0 6\0"
-
-#define DFU_ALT_INFO_NOR \
-	"dfu_alt_info_nor="		    \
-	"uboot raw 0xA000 0x56000;" \
-	"dtb raw 0x70000 0x10000" \
-	"\0"
-
-#define CONFIG_EXTRA_ENV_SETTINGS \
-	"altbootcmd=setenv recovery_available 0; setenv bootcmd; saveenv\0" \
-	"recovery_available=1\0" \
-    "bootlimit=3\0" \
-	KUBOS_CURR_VERSION "=" KUBOS_BASE "\0" \
-	KUBOS_PREV_VERSION "=" KUBOS_BASE "\0" \
-	KUBOS_CURR_TRIED "=0\0" \
-	DFU_ALT_INFO_MMC \
-	DFU_ALT_INFO_NOR
-
-#else
-
-#define CONFIG_EXTRA_ENV_SETTINGS \
-	"altbootcmd=setenv recovery_available 0; setenv bootcmd; saveenv\0" \
-	"recovery_available=1\0" \
-    "bootlimit=1\0" \
-
-#endif /* CONFIG_UPDATE_KUBOS */
-#endif /* Placeholder */
